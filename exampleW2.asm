64ca6f98b7257a68916233949409f3ee4aefbb7d
i-ii-iii-iv-v <iiiiiiivv54321@gmail.com>
2017-10-11 21:18:00 -0700

    finished example2

--- exampleW2.asm
+++ exampleW2.asm
@@ -3,12 +3,12 @@ int sub(int x, int y){
 00EF1020  push        ebp  
 00EF1021  mov         ebp,esp  
 	return 2*x+y;
-00EF1023  mov         eax,dword ptr [ebp+8]  
-00EF1026  mov         ecx,dword ptr [ebp+0Ch]  
-00EF1029  lea         eax,[ecx+eax*2]  
+00EF1023  mov         eax,dword ptr [ebp+8]  ;move  x to eax
+00EF1026  mov         ecx,dword ptr [ebp+0Ch]  ;move y to ecx
+00EF1029  lea         eax,[ecx+eax*2]  ;2*x+y, move in eax without dereferencing the addr
 }
-00EF102C  pop         ebp  
-00EF102D  ret  
+00EF102C  pop         ebp  ;get back caller stack frame
+00EF102D  ret  			   ;return using address on the stack, pop the ret address of the stack
 
 
 int main(int argc, char ** argv){
@@ -24,15 +24,15 @@ int main(int argc, char ** argv){
 00EF103A  push        ecx  ;push argv[1] onto stack. pointer to second argument
 00EF103B  call        dword ptr ds:[00EF6238h]  ;call to atoi, return value at eax
 00EF1041  add         esp,4  ;cdecl, caller cleans up the stack parameters
-00EF1044  mov         dword ptr [ebp-4],eax  ; store return value into variable allocated in 00EF1033
+00EF1044  mov         dword ptr [ebp-4],eax  ; store return value into int a
 	return sub(argc,a);
-00EF1047  mov         edx,dword ptr [ebp-4]  
-00EF104A  push        edx  
-00EF104B  mov         eax,dword ptr [ebp+8]  
-00EF104E  push        eax  
-00EF104F  call        00EF1005  
-00EF1054  add         esp,8  
+00EF1047  mov         edx,dword ptr [ebp-4]  ;move value in int a to edx
+00EF104A  push        edx  					 ;push edx as an argument to sub  argument right to left
+00EF104B  mov         eax,dword ptr [ebp+8]  ;push argc into eax
+00EF104E  push        eax  				     ;push eax as an 1st argument
+00EF104F  call        00EF1005  			 ;call sub
+00EF1054  add         esp,8  				 ;cdecl, caller cleans up the parameters in the stack
 }
-00EF1057  mov         esp,ebp  
-00EF1059  pop         ebp  
-00EF105A  ret  
+00EF1057  mov         esp,ebp  				 ;move stack pointer to base of main stack frame (deletes everything off the stack quickly)
+00EF1059  pop         ebp  					 ;restore base for caller function of main()'s stack frame
+00EF105A  ret  								 ;return to whatever called main
